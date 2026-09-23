2edf622af13e758d11545a8b69f30d47fb9e7901
buonsorgio <161519601+buonsorgio@users.noreply.github.com>
2026-09-23 19:57:18 +0200

    Remove plugin and dependency management sections

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,1 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "EnricoPapi"
-include(":app")
+file finto
